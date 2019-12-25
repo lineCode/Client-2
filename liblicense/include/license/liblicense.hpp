aabf0f307a1c8681817080bba4e2be131e24e92f
Chris Ward <chrisward9000@gmail.com>
2019-12-25 20:56:39 +0000

    licensing

---
+++ liblicense/include/license/liblicense.hpp
@@ -0,0 +1,19 @@
+// licensesdk.hpp
+//
+
+#ifndef IDZT4PMOL91GIMNA5HZVIHZE2B0VYX6BDG
+#define IDZT4PMOL91GIMNA5HZVIHZE2B0VYX6BDG
+
+///// Includes /////
+
+#include "licensekey.hpp"
+#include "productkey.hpp"
+
+///// Namespaces /////
+
+namespace license
+{
+
+}
+
+#endif
