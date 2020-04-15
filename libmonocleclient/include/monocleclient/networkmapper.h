e05c3176a69b8fc4489120734ad918f7cad5e4fb
Chris Ward <chris@monoclesecurity.com>
2020-04-15 08:01:34 +0100

    fixing

--- libmonocleclient/include/monocleclient/networkmapper.h
+++ libmonocleclient/include/monocleclient/networkmapper.h
@@ -6,7 +6,11 @@
 
 ///// Includes /////
 
+#include <boost/shared_ptr.hpp>
+#include <mutex>
+#include <onvifclient/deviceclient.hpp>
 #include <QObject>
+#include <vector>
 
 ///// Namespaces /////
 
@@ -23,6 +27,42 @@ class NetworkMapper : public QObject
 
   NetworkMapper();
   ~NetworkMapper();
+
+ protected:
+
+  virtual void timerEvent(QTimerEvent*) override;
+
+ private:
+
+  class Scanner
+  {
+   public:
+
+    Scanner(const boost::shared_ptr<std::recursive_mutex>& mutex, const uint8_t a, const std::pair<uint8_t, uint8_t>& b, const std::pair<uint8_t, uint8_t>& c, const std::pair<uint8_t, uint8_t>& d, const size_t maxconnections);
+
+    void Update();
+
+   private:
+
+    std::vector<uint8_t> CreateRange(const std::pair<uint8_t, uint8_t>& inputs) const; // <start, end> inclusive
+
+    boost::shared_ptr<std::recursive_mutex> mutex_;
+    const uint8_t a_;
+    const std::pair<uint8_t, uint8_t> b_;
+    const std::pair<uint8_t, uint8_t> c_;
+    const std::pair<uint8_t, uint8_t> d_;
+    const size_t maxconnections_;
+    std::vector<uint8_t> currentb_;
+    std::vector<uint8_t> currentc_;
+    std::vector<uint8_t> currentd_;
+
+    std::vector< boost::shared_ptr<onvif::device::DeviceClient> > connections_;
+
+  };
+
+  boost::shared_ptr<std::recursive_mutex> mutex_;
+
+  std::vector<Scanner> scanners_;
 
  signals:
 
--- libmonocleclient/src/networkmapper.cpp
+++ libmonocleclient/src/networkmapper.cpp
@@ -9,14 +9,78 @@
 #include <boost/asio/ip/address.hpp>
 #include <utility/utility.hpp>
 #include <QDebug>//TODO
+
+#include "monocleclient/mainwindow.h"
+
 ///// Namespaces /////
 
 namespace client
 {
 
+///// Globals /////
+
+const size_t MAX_CONNECTIONS = 20;//TODO maybe more?
+
 ///// Methods /////
 
-NetworkMapper::NetworkMapper()
+NetworkMapper::Scanner::Scanner(const boost::shared_ptr<std::recursive_mutex>& mutex, const uint8_t a, const std::pair<uint8_t, uint8_t>& b, const std::pair<uint8_t, uint8_t>& c, const std::pair<uint8_t, uint8_t>& d, const size_t maxconnections) :
+  mutex_(mutex),
+  a_(a),
+  b_(b),
+  c_(c),
+  d_(d),
+  maxconnections_(maxconnections),
+  currentb_(CreateRange(b)),
+  currentc_(CreateRange(c)),
+  currentd_(CreateRange(d))
+{
+
+}
+
+void NetworkMapper::Scanner::Update()
+{
+  while ((connections_.size() < maxconnections_) || (currentb_.size() && currentc_.size() && currentd_.size()))
+  {
+    //TODO steal an ip address..
+
+
+    const std::string uri = "http://" + + "/onvif/device_service";
+
+
+
+    boost::shared_ptr<onvif::device::DeviceClient> connection = boost::make_shared<onvif::device::DeviceClient>(mutex_);//TODO need to call update on all of these...
+    if (connection->Init(sock::ProxyParams(), uri, "username", "password", 1, false, true))
+    {
+      // This should never happen, but lets crack on...
+      continue;
+    }
+
+    //TODO now kick off
+
+    connections_.push_back(connection);//TODO might need to mutex this?
+  }
+
+  for (const boost::shared_ptr<onvif::device::DeviceClient>& connection : connections_)
+  {
+    connection->Update();//TODO if this fails do we remove it?
+
+  }
+}
+
+std::vector<uint8_t> NetworkMapper::Scanner::CreateRange(const std::pair<uint8_t, uint8_t>& inputs) const
+{
+  std::vector<uint8_t> range;
+  range.reserve((inputs.second - inputs.first) + 1);
+  for (uint8_t i = inputs.first; i <= inputs.second; ++i)
+  {
+    range.push_back(i);
+
+  }
+  return range;
+}
+
+NetworkMapper::NetworkMapper() :
+  mutex_(boost::make_shared<std::recursive_mutex>())
 {
   const std::pair< int, std::vector<utility::ADDRESS> > addresses = utility::GetAddresses();
   if (addresses.first)
@@ -45,14 +109,17 @@ NetworkMapper::NetworkMapper()
       continue;
     }
 
+    //TODO pass in the ports as well... we want http on 80,8888,8080,88 and https on 443
     if (boost::starts_with(address.address_, "10."))
     {
       classa = true;
+      //TODO we want the subnet mask so we can decide how much we want to fiddle...
 
     }
     else if (boost::starts_with(address.address_, "192.168."))
     {
       classc = true;
+      //TODO we want the subnet mask so we can decide how much we want to fiddle...
 
     }
     else if (boost::starts_with(address.address_, "169.254."))
@@ -60,16 +127,41 @@ NetworkMapper::NetworkMapper()
       linklocal = true;
 
     }
-    qDebug() << QString::fromStdString(address.address_);
   }
 
+  if (classa)
+  {
+    scanners_.push_back(Scanner(mutex_, 10, { 0, 255 }, { 0, 255 }, { 1, 255 }, MAX_CONNECTIONS));
 
+  }
 
+  if (classc)
+  {
+    scanners_.push_back(Scanner(mutex_, 192, { 168, 168 }, { 0, 255 }, { 1, 255 }, MAX_CONNECTIONS));
+
+  }
+
+  if (linklocal)
+  {
+    scanners_.push_back(Scanner(mutex_, 169, { 254, 168 }, { 0, 255 }, { 1, 255 }, MAX_CONNECTIONS));
+
+  }
+
+  startTimer(std::chrono::milliseconds(100));
 }
 
 NetworkMapper::~NetworkMapper()
 {
 
+}
+
+void NetworkMapper::timerEvent(QTimerEvent*)
+{
+  for (Scanner& scanner : scanners_)
+  {
+    scanner.Update();
+
+  }
 }
 
 }
